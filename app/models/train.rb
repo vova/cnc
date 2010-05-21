3af1f4411558fee2b8fb92c40967bfd18dd70e9e
Serge Balyuk <silver@barracuda.(none)>
2010-05-21 21:46:41 +0300

    syntax fixes

--- app/models/train.rb
+++ app/models/train.rb
@@ -35,11 +35,11 @@ class Train < ActiveRecord::Base
     pos = distance_from_base_station
     stations_from_0 = station_distances.select {|d| d < pos}
     
-    return 0, 0
     line_stations = STATIONS.select {|s| s[:color] == line_name}
     point1 = line_stations[stations_from_0.size]
     point2 = line_stations[stations_from_0.size + 1]
     coords = train_coordinates(point1[:coordinates], point2[:coordinates], pos - stations_from_0.last)
+    logger.error "-------------------------- #{coords.inspect}"
     {:lat => coords.first, :lng => coords.last}
     # TODO remove stub
     # line_name, index of station, distance from that station. should return {:lat => lat, :lng => lng}
@@ -48,12 +48,8 @@ class Train < ActiveRecord::Base
   end
 
   def train_coordinates(point1, point2, train_dist)
-    delta = train_dist / dist(point1, point2)
+    delta = train_dist / Stations.get_distance_between(point1, point2)
     [ point1[:lat] + (point2[:lat]-point1[:lat]) * delta, point1[:lng] + (point2[:lng]-point1[:lng]) * delta ]
   end
-
-end
-
-
 
 end
